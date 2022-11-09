d7c8f08f154cd71cfffbdca78f504864cacac425
victorazangu <victorsazangu@gmail.com>
2022-11-10 00:04:39 +0300

    the project 2

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::post('register',[AuthController::class,'register'])->name('register');
+Route::post('login',[AuthController::class,'login'])->name('login');
+
+Route::middleware(['auth:api','active_user','verified'])->group( function () {
+    // our routes that are protected
+
+    //user protected routes 
+    Route::get('profile',[AuthController::class,'profile']);
+    Route::get('logout',[AuthController::class,'logout'])->name('logout');
+ 
 });
